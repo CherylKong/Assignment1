8d345e20783ec7939f41d3c38798880c323a8db1
Cheryl Kong <ykong13@my.bcit.ca>
2020-11-28 23:20:04 -0800

    Change the to a darker color when darkTheme button is clicked, and its text change to light Theme

---
+++ assignment2/script/script.js
@@ -0,0 +1,83 @@
+let dark_theme = document.querySelector('.theme')
+let dark_theme_color = ['#707070', '#c9c1c1', 'black', 'green','#393e46', 'green','red']
+let light_theme_color = ['#afadad', '#eae6e6','white', '#8db596','#707070', '#8db596', '#ec524b'] 
+function change_dark_theme()
+{
+    let variable = []
+
+    let side_note = document.querySelector('.notes')
+    variable.push(side_note)
+    // side_note.style.backgroundColor = '#707070'
+    // change_font_color_to_white(side_note)
+
+    let new_note = document.querySelector('.new_note')
+    variable.push(new_note)
+    // new_note.style.backgroundColor = '#c9c1c1'
+    // change_font_color_to_white(new_note)
+
+    let text_area = document.querySelector('.contain')
+    variable.push(text_area)
+    // text_area.style.backgroundColor = 'black'
+
+    let button1 = document.querySelector('.button1')
+    variable.push(button1)
+    // button1.style.backgroundColor = 'green'
+
+    // dark_theme.style.backgroundColor = '#393e46'
+    let button2 = document.querySelector('.button2')
+    let text = change_theme_text(button2)
+    dark_theme.textContent = text
+    variable.push(dark_theme)
+
+    let button3 = document.querySelector('.button3')
+    variable.push(button3)
+    // button3.style.backgroundColor = 'green'
+
+    let button4 = document.querySelector('.button4')
+    variable.push(button4)
+    // button4.style.backgroundColor = 'red'
+    change_theme(text, variable)
+}
+
+function change_theme_text(theme_text)
+{
+    let text = theme_text.innerText
+    if(text === "Light Theme")
+    {
+        text = "Dark Theme"
+    }
+    else
+    {
+        text = "Light Theme"
+    }
+    return text
+}
+function change_theme(text,variable)
+{
+    if (text === "Light Theme")
+    {
+        for (let index in variable)
+        {
+            variable[index].style.backgroundColor = dark_theme_color[index]
+            variable[index].style.color = 'white'
+        }
+    
+    }
+    else
+    {
+        for (let index in variable)
+        {
+           variable[index].style.backgroundColor = light_theme_color[index] 
+           variable[index].style.color = 'black'
+        }
+    
+    }
+    dark_theme.addEventListener('click',change_dark_theme)
+}
+function change_font_color_to_white(posiiton, color)
+{
+    posiiton.style.color = color
+}
+
+dark_theme.addEventListener('click',change_dark_theme)
+
